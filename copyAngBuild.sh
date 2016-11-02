9a272650bdf8c7921d3cf9b37592a4237efdba7d
joshuaforman <joshuajforman@gmail.com>
2016-11-02 10:02:33 -0600

    created build for DO server

--- copyAngBuild.sh
+++ copyAngBuild.sh
@@ -1,3 +1,6 @@
+cd familygenie-angular
+npm run build
+cd ..
 rm -r familygenie-angserver/public/*
 cp -r familygenie-angular/dist/* familygenie-angserver/public/
 cp -r familygenie-angular/assets/* familygenie-angserver/public/assets
