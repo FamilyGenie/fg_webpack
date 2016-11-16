c6537460e18387e9b590aff95edac4c474bd5024
joshuaforman <joshuajforman@gmail.com>
2016-11-16 11:31:05 -0700

    import of parents and pairbonds now working too

--- familygenie-apiserver/gedcom/gedcom.js
+++ familygenie-apiserver/gedcom/gedcom.js
@@ -29,7 +29,7 @@ module.exports = function(app, mongoose, bodyParser, passport) {
     else {
       console.log('gedcom saved and parsed to json with python');
       // this call imports the file that was just uploaded into mongoDB
-      exec('mongoimport --db test --collection gedcom_indi --type json --file ./gedcom/jsonfiles/' + req.file.filename + 'indi.json --jsonArray', function(err) { 
+      exec('mongoimport --db test --collection gedcom_people --type json --file ./gedcom/jsonfiles/' + req.file.filename + 'indi.json --jsonArray', function(err) { 
         if(err) {
           console.log('mongo import failed', err);
         }
@@ -40,58 +40,46 @@ module.exports = function(app, mongoose, bodyParser, passport) {
     }
     });
 
-  //   // parse and import parents
-  //   exec('python ./gedcom/gedcomparent.py ./gedcom/uploads/' + req.file.filename + ' ./gedcom/jsonfiles/' + req.file.filename + 'parent.json',  // run the python program on the info
-  //     function(err) {
-  //     if(err) {
-  //       console.log('python parse failed', err);
-  //     }
-  //     else {
-  //       console.log('gedcom saved and parsed to json with python');
-  //       exec('mongoimport --db test --collection gedcom_parent --type json --file ./gedcom/jsonfiles/' + req.file.filename + 'parent.json --jsonArray', function(err) { // imports the file that was just uploaded into mongoDB
-  //         if(err) {
-  //           console.log('mongo import failed', err);
-  //         }
-  //         else {
-  //           console.log('json file imported to mongo');
-  //         }
-  //       });
-  //     }
-  //   });
+    // parse and import parents
+    exec('python ./gedcom/gedcomparent.py ./gedcom/uploads/' + req.file.filename + ' ./gedcom/jsonfiles/' + req.file.filename + 'parent.json',  // run the python program on the info
+      function(err) {
+      if(err) {
+        console.log('python parse failed', err);
+      }
+      else {
+        console.log('gedcom saved and parsed to json with python');
+        exec('mongoimport --db test --collection gedcom_parents --type json --file ./gedcom/jsonfiles/' + req.file.filename + 'parent.json --jsonArray', function(err) { // imports the file that was just uploaded into mongoDB
+          if(err) {
+            console.log('mongo import failed', err);
+          }
+          else {
+            console.log('json file imported to mongo');
+          }
+        });
+      }
+    });
 
-  //   // parse and import pair bonds
-  //   exec('python ./gedcom/gedcompairbonds.py ./gedcom/uploads/' + req.file.filename + ' ./gedcom/jsonfiles/' + req.file.filename + 'pairbond.json',  // run the python program on the info
-  //     function(err) {
-  //     if(err) {
-  //       console.log('python parse failed', err);
-  //     }
-  //     else {
-  //       console.log('gedcom saved and parsed to json with python');
-  //       exec('mongoimport --db test --collection gedcom_pairbond --type json --file ./gedcom/jsonfiles/' + req.file.filename + 'pairbond.json --jsonArray', function(err) { // imports the file that was just uploaded into mongoDB
-  //         if(err) {
-  //           console.log('mongo import failed', err);
-  //         }
-  //         else {
-  //           console.log('json file imported to mongo');
-  //         }
-  //       });
-  //     }
-  //   });
-
-  //   res.redirect('/uploaddirect');
-  // });
-
+    // parse and import pair bonds
+    exec('python ./gedcom/gedcompairbonds.py ./gedcom/uploads/' + req.file.filename + ' ./gedcom/jsonfiles/' + req.file.filename + 'pairbond.json',  // run the python program on the info
+      function(err) {
+      if(err) {
+        console.log('python parse failed', err);
+      }
+      else {
+        console.log('gedcom saved and parsed to json with python');
+        exec('mongoimport --db test --collection gedcom_pairbond --type json --file ./gedcom/jsonfiles/' + req.file.filename + 'pairbond.json --jsonArray', function(err) { // imports the file that was just uploaded into mongoDB
+          if(err) {
+            console.log('mongo import failed', err);
+          }
+          else {
+            console.log('json file imported to mongo');
+          }
+        });
+      }
+    });
   });
 
-
-
-  // app.use(function(req, res, next) {
-  //     console.log(req.url);
-  //     next();
-  // });
-
-  // app.use(express.static(__dirname));
-
+  // this should not be needed any more. Keeping around if needed for troubleshooting
   app.get("/uploaddirect", function(req, res) {
       res.sendFile(__dirname + "/upload.html");
   });
